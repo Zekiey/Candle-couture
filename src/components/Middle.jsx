20936d41bf989242492a325be40d87abb20156b5
Zekiey <110130958+Zekiey@users.noreply.github.com>
2024-01-30 15:52:11 +0100

    final updates ijn

--- src/components/Middle.jsx
+++ src/components/Middle.jsx
@@ -18,28 +18,28 @@ const Middle = () => {
                 </div>
                 <img className='max-[320px]:h-1 max-[320px]:w-10 max-[375px]:w-40 max-[425px]:w-40 max-[425px]:h-4 max-[375px]:h-4' src={line2} alt="" />
             </div>
-            <div className='flex max-[320px]:flex-col max-[375px]:flex-col  max-[375px]:h-auto  max-[320px]:h-auto  max-[320px]:space-y-5 max-[375px]:space-y-5  text-black space-x-10 justify-center max-[375px]:items-center max-[425px]:items-center mt-10'>
-                <div className='bg-[#FDF0CD] flex rounded-md  items-center space-x-5 pr-5 max-[320px]:pr-1 max-[320px]:w-60 max-[375px]:w-96 max-[320px]:ml-10 max-[375px]:ml-10 pt-2 pb-2 hover:-translate-y-3 hover:transition hover:duration-300'>
-                    <img className='w-5 max-[375px]:w-10 h-5 max-[375px]:h-10 -mt-10 max-[425px]:w-5' src={clipboard} alt="" />
+            <div className='flex max-[320px]:flex-col max-[375px]:flex-col max-[425px]:flex-col   max-[375px]:h-auto max-[425px]:h-auto max-[320px]:h-auto  max-[320px]:space-y-5 max-[375px]:space-y-5 max-[425px]:space-y-10  text-black space-x-10 justify-center max-[375px]:items-center max-[425px]:items-center max-[425px]:flex max-[425px]:justify-center mt-10'>
+                <div className='bg-[#FDF0CD] flex  rounded-md max-[425px]:w-96  items-center space-x-5 max-[425px]:space-x-9 pr-5 max-[320px]:pr-1 max-[320px]:w-60 max-[375px]:w-96 max-[320px]:ml-10 max-[375px]:ml-10 max-[425px]:ml-9 pt-2 pb-2 hover:-translate-y-3 hover:transition hover:duration-300'>
+                    <img className='w-5 max-[375px]:w-10 h-5 max-[375px]:h-10 -mt-12 max-[425px]:w-5' src={clipboard} alt="" />
                     <div>
                         <h1 className='font-Playfair text-[20px] max-[375px]:text-[40px] max-[425px]:text-[20px] font-semibold italic'>Newsletter</h1>
                         <p className='font-Sora text-[12px] max-[375px]:text-[20px] max-[425px]:text-[16px]'>Join hundreds of others <br /> finding light in their daily lives.</p>
                     </div>
                     <img className='w-5 h-5 max-[375px]:w-10  max-[375px]:h-10' src={add} alt="" />
                 </div>
-                <div className='bg-[#FDF0CD] flex rounded-md  items-center space-x-5 pr-5 max-[320px]:pr-1 max-[320px]:w-60 max-[375px]:w-96 max-[320px]:pt-8 max-[375px]:pt-8 max-[320px]:pb-5 max-[375px]:pb-5 pt-2 pb-2 hover:-translate-y-3 hover:transition hover:duration-300'>
-                    <img className='w-5 h-5 max-[375px]:w-10  max-[375px]:h-10  -mt-10' src={task} alt="" />
-                    <div className='-mt-5 max-[425px]:-mt-5'>
-                        <h1 className='font-Playfair text-[20px] font-semibold italic max-[375px]:text-[40px] max-[425px]:text-[20px]'>Guides</h1>
-                        <p className='font-Sora text-[12px] max-[375px]:text-[20px] max-[425px]:text-[16px]'>Lorem ipsum lorem</p>
+                <div className='bg-[#FDF0CD] flex rounded-md max-[425px]:h-auto max-[425px]:w-68  items-center space-x-5 pr-5 max-[320px]:pr-1 max-[320px]:w-60 max-[425px]:mt-10  max-[425px]:pt- max-[375px]:w-96 max-[320px]:pt-8 max-[375px]:pt-8 max-[320px]:pb-5 max-[375px]:pb-5 pt-2 pb-2 hover:-translate-y-3 hover:transition hover:duration-300'>
+                    <img className='w-5 h-5 max-[375px]:w-10  max-[375px]:h-10  -mt-8' src={task} alt="" />
+                    <div className='-mt-5 max-[425px]:mt-5 '>
+                        <h1 className='font-Playfair text-[20px] font-semibold italic max-[375px]:text-[40px] max-[425px]:text-[20px] '>Guides</h1>
+                        <p className='font-Sora text-[12px] max-[375px]:text-[20px] max-[425px]:text-[16px]'>Lorem ipsum lorem, sit repudiandae <br /> architecto quasicorrupti et?</p>
                     </div>
                     <p className='font-Sora text-[8px] max-[375px]:text-[20px] max-[425px]:text-[16px] bg-gradient-to-b max-[375px]:w-auto  max-[375px]:h-auto from-[rgb(233,189,71,0.909)] to-[rgb(120,88,0,0.8706)]'>Coming soon</p>
                 </div>
-                <div className='bg-[#FDF0CD] flex rounded-md  items-center max-[425px]:justify-center space-x-5 pr-5 max-[320px]:pr-1 max-[320px]:w-60 max-[375px]:w-96 max-[320px]:pt-8 max-[375px]:pt-8 max-[320px]:pb-5 max-[375px]:pb-5 pt-2 pb-2 hover:-translate-y-3 hover:transition hover:duration-300'>
-                    <img className='w-5 h-5  max-[375px]:w-10  max-[375px]:h-10 -mt-10' src={device} alt="" />
-                    <div className='-mt-5'>
+                <div className='bg-[#FDF0CD] flex rounded-md max-[425px]:h-auto max-[425px]:items-center max-[425px]:justify-center space-x-5 pr-5 max-[320px]:pr-1 max-[320px]:w-60 max-[375px]:w-96 max-[320px]:pt-8 max-[375px]:pt-8 max-[320px]:pb-5 max-[375px]:pb-5 pt-2 pb-2 hover:-translate-y-3 hover:transition hover:duration-300'>
+                    <img className='w-5 h-5  max-[375px]:w-10  max-[375px]:h-10 -mt-8' src={device} alt="" />
+                    <div className='-mt-5 max-[425px]:mt-5'>
                         <h1 className='font-Playfair text-[20px] font-semibold italic max-[375px]:text-[40px] max-[425px]:text-[20px]'>Prayer Request</h1>
-                        <p className='font-Sora text-[12px] max-[375px]:text-[20px] max-[425px]:text-[16px]'>Lorem ipsum lorem</p>
+                        <p className='font-Sora text-[12px] max-[375px]:text-[20px] max-[425px]:text-[16px]'>Lorem ipsum  Lorem ipsum dolor sit amet <br /> llo adipisci sunt natus molestias iusto architecto </p>
                     </div>
                     <img className='w-5 h-5 max-[375px]:w-10  max-[375px]:h-10 ' src={add} alt="" />
                 </div>
